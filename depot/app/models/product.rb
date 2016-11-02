b79d18278933b25ef05d515fd5ac1abdc85f8a42
dyzheng <dyzheng@freewheel.tv>
2016-11-02 16:17:39 +0800

    Chapter 9

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
- default_scope { order('title') }
+  default_scope { order('title') }
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
@@ -13,4 +15,15 @@ class Product < ActiveRecord::Base
       :minimum => 10,
 	  :message => 'must be longer than 10 characters'
 	}
+
+  private
+    # ensure that there are noline items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
