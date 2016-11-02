f906441ac97f2711ce51a2498c5134b2365a651d
dyzheng <dyzheng@freewheel.tv>
2016-11-02 16:36:24 +0800

    Chapter 9

---
+++ depot/app/controllers/store_controller.rb
@@ -0,0 +1,10 @@
+class StoreController < ApplicationController
+  def index
+  	@products = Product.all
+  	if session[:counter].nil?
+  	  session[:counter] = 1
+  	else
+  	  session[:counter] += 1
+  	end
+  end
+end
